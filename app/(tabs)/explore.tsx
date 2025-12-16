4d6aaf61cc8e9f28c285d5955dabe6b089550680
JoshxB-T <joshtorrez320@gmail.com>
2025-12-16 13:36:18 -0600

    Updated Explore page background image

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,12 +1,11 @@
 import { Image } from 'expo-image';
 import { Platform, StyleSheet } from 'react-native';
 
-import { Collapsible } from '@/components/ui/collapsible';
 import { ExternalLink } from '@/components/external-link';
 import ParallaxScrollView from '@/components/parallax-scroll-view';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
-import { IconSymbol } from '@/components/ui/icon-symbol';
+import { Collapsible } from '@/components/ui/collapsible';
 import { Fonts } from '@/constants/theme';
 
 export default function TabTwoScreen() {
@@ -14,10 +13,8 @@ export default function TabTwoScreen() {
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
       headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="chevron.left.forwardslash.chevron.right"
+        <Image
+          source={require('@/assets/images/ds1.jpg')}
           style={styles.headerImage}
         />
       }>
@@ -108,5 +105,13 @@ const styles = StyleSheet.create({
   titleContainer: {
     flexDirection: 'row',
     gap: 8,
+  },
+  headerImage: {
+    height: 500,
+    width: 445,
+    bottom: 0,
+    left: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
